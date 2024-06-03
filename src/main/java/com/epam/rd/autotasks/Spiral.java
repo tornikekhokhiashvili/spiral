a45fd77c6bfdfbb1bd16159f612df24227c580ca
tornikekhokhiashvili <tornikexoxiashvili@gmail.com>
2024-06-03 23:00:04 +0400

    Initial Commit

--- src/main/java/com/epam/rd/autotasks/Spiral.java
+++ src/main/java/com/epam/rd/autotasks/Spiral.java
@@ -2,6 +2,32 @@ package com.epam.rd.autotasks;
 
 class Spiral {
     static int[][] spiral(int rows, int columns) {
-        return null;
+        int[][] result = new int[rows][columns];
+        int count = 1;
+        int rowStart = 0, rowEnd = rows - 1, columnStart = 0, columnEnd = columns - 1;
+        while (rowStart <= rowEnd && columnStart <= columnEnd) {
+            for (int i = columnStart; i <= columnEnd; i++) {
+                result[rowStart][i] = count++;
+            }
+            rowStart++;
+            for (int i = rowStart; i <= rowEnd; i++) {
+                result[i][columnEnd] = count++;
+            }
+            columnEnd--;
+            if (rowStart <= rowEnd) {
+                for (int i = columnEnd; i >= columnStart; i--) {
+                    result[rowEnd][i] = count++;
+                }
+                rowEnd--;
+            }
+
+            if (columnStart <= columnEnd) {
+                for (int i = rowEnd; i >= rowStart; i--) {
+                    result[i][columnStart] = count++;
+                }
+                columnStart++;
+            }
+        }
+        return result;
     }
 }
